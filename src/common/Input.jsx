aeea926d2e4da9ba970887239e22effe40e0abcd
unknown <deepakradialcode@gmail.com>
2024-12-07 09:39:00 +0530

    padding decrease

--- src/common/Input.jsx
+++ src/common/Input.jsx
@@ -6,7 +6,7 @@ const Input = ({placeholder}) => {
       <div>
           <form action="#">
               <label htmlFor="address" className='flex items-center w-full bg-white rounded-tr-[50px] rounded-br-[50px] rounded-bl-[50px] pr-[9px] max-md:pr-1 border border-gray-400 overflow-hidden '>
-                  <input id='address' type="text" placeholder={placeholder} className='w-full h-full py-6 max-md:py-3 pl-[22px] font-helvetica text-base max-sm:text-sm text-black outline-none'  />
+                  <input id='address' type="text" placeholder={placeholder} className='w-full h-full py-6 max-md:py-3 pl-[22px] max-sm:pl-3 font-helvetica text-base max-sm:text-sm text-black outline-none'  />
                   <Button text='Compare' color='text-white' bg='bg-skyBlue' />
               </label>
           </form>
